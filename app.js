1c1b30f1b3c65df7da51ec49de30b0bcdef55e61
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-05-18 10:27:36 -0400

    fix bugs

--- app.js
+++ app.js
@@ -13,21 +13,15 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/inventoryDB", {useNewUrlParser: true});
 
+// Creating schemas and collections for DB
+
 const itemsSchema = new mongoose.Schema({
   name: String
 });
 
 const Item = mongoose.model("Item", itemsSchema);
 
-const item1 = new Item({
-  name: "⬅  Check this box to delete an item."
-});
-
-const item2 = new Item({
-  name: "Hit '➕' below to add a new item."
-});
-
-const defaultItems = [item1, item2];
+const defaultItems = [];  // creating an empty array so that it can be assigned to 'items' property of the newly created Location and it won't have its value as null
 
 const locationSchema = new mongoose.Schema({
   name: String,
@@ -36,30 +30,24 @@ const locationSchema = new mongoose.Schema({
 
 const Location = mongoose.model("Location", locationSchema);
 
+// ROUTES
+
 app.get("/", function(req, res) {
-  
-  Item.find({}, function(err, foundItems) {
-    if(foundItems.length === 0) {
-      Item.insertMany(defaultItems, function(err) {
-        if(err) {
-          console.log(err);
-        } else {
-          console.log("Successfully saved defaultItems to DB!");
-        }
-      });
-      res.redirect("/");
-    } else {
-      res.render("list", {locationTitle: "Example", newListItems: foundItems});
-    }
+
+  Item.find({}, function(err, foundItems) {    // foundItems gives you back the array of items
+    res.render("list", {locationTitle: "Example", newListItems: foundItems});
   });
 });
 
 app.get("/:customLocationName", function(req, res) {
+  
   const customLocationName = _.capitalize(req.params.customLocationName);
 
-  Location.findOne({name: customLocationName}, function(err, foundLocation) {
-    if(!err) {
-      if(!foundLocation) {
+  Location.findOne({name: customLocationName}, function(err, foundLocation) {   // foundLocaton gives you the object with 'name'/'items' properties
+    if(err) {
+      console.log(err);
+    } else {
+      if(foundLocation === null) {      // Got to be careful while checking whether something equals null/undefined
         const location = new Location({
           name: customLocationName,
           items: defaultItems
@@ -69,53 +57,20 @@ app.get("/:customLocationName", function(req, res) {
       } else {
         res.render("list", {locationTitle: foundLocation.name, newListItems: foundLocation.items});
       }
-    } else {
-      console.log(err);
     }
   });
 });
 
-app.post("/:customLocationName", function(req, res) {
-  if(req.body.locationName) {
-    const customLocationName = req.body.locationName;
-    if(customLocationName === "Example") {
-      res.redirect("/");
-    } else {
-      res.redirect("/" + customLocationName);
-    }
-  } else {
-    const itemName = req.body.newItem;
-    const locationName = req.body.location;
-
-    const item = new Item ({
-      name: itemName
-    });
-
-    if(locationName === "Example") {
-      item.save();
-      res.redirect("/");
-    } else {
-      Location.findOne({name: locationName}, function(eror, foundLocation){
-        foundLocation.items.push(item);
-        foundLocation.save();
-        res.redirect("/" + locationName);
-      });
-    }
-  }
-});
-
 app.post("/", function(req, res) {
-  
-  if(req.body.locationName) {
-    const customLocationName = req.body.locationName;
-    if(customLocationName === "Example") {
-      res.redirect("/");
-    } else {
-      res.redirect("/" + customLocationName);
-    }
-  } else {
-    const itemName = req.body.newItem;
-    const locationName = req.body.location;
+
+  if(req.body.newItem === undefined) {     // Got to be careful while checking whether something equals null/undefined
+    const customLocationName = req.body.locationName; // locationName comes from header.ejs
+    res.redirect("/" + customLocationName); 
+  } 
+  if(req.body.locationName === undefined) {
+    
+    const itemName = req.body.newItem;     // newItem comes from list.ejs 2nd <form>
+    const locationName = req.body.location;   // location comes from list.ejs 2nd <form>
 
     const item = new Item ({
       name: itemName
@@ -128,7 +83,7 @@ app.post("/", function(req, res) {
       Location.findOne({name: locationName}, function(eror, foundLocation){
         foundLocation.items.push(item);
         foundLocation.save();
-        res.redirect("/" + locationName);
+        res.render("list", {locationTitle: foundLocation.name, newListItems: foundLocation.items});
       });
     }
   }
